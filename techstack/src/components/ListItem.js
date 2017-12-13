cf7179e2f04db8e4ab4b85adb47a8a736c737a9f
Sam Thomson <samt@samt.st>
2017-12-13 14:23:30 +0700

    listview for library items

---
+++ techstack/src/components/ListItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import { CardSection } from './common'
+
+class ListItem extends Component {
+
+    render() {
+        const { title } = this.props.library
+        const { titleStyle } = styles
+
+        return (
+            <CardSection>
+                <Text style={titleStyle}>{title}</Text>
+            </CardSection>
+        )
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+}
+
+export default ListItem
